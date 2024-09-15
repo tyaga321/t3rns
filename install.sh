7099a4e90f578bbac67abbb3070547d086af3f1d
tyaga321 <58379335+tyaga321@users.noreply.github.com>
2024-09-15 23:21:57 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-echo "Showing HCA logo..."
-wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/loader.sh && chmod +x loader.sh && ./loader.sh
-curl -s https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/logo.sh | bash
-sleep 2
+sleep 1
 
 echo "Welcome to the t3rn ALE!"
 
