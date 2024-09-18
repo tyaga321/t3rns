5279963643c4bc306c7b465657e83adcc6ea34fc
tyaga321 <58379335+tyaga321@users.noreply.github.com>
2024-09-18 16:05:42 +0700

    Update run_executor2.sh

--- run_executor2.sh
+++ run_executor2.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 while true; do
-    cd t3rn2/executor/executor/bin
-    sleep 1
-    ./executor
+    cd t3rn2/executor/executor/bin && ./executor
     sleep 5  # Wait 5 seconds before restarting
 done
