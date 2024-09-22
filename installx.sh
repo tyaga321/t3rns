ac745d9126cfcaf832fcc357f323a589d01dd460
tyaga321 <58379335+tyaga321@users.noreply.github.com>
2024-09-22 23:59:06 +0700

    Update installx.sh

--- installx.sh
+++ installx.sh
@@ -9,8 +9,8 @@ sudo apt -q update
 sudo apt -qy upgrade
 
 
-EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.19.0/executor-linux-v0.19.0.tar.gz"
-EXECUTOR_FILE="executor-linux-v0.19.0.tar.gz"
+EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.21.1/executor-linux-v0.21.1.tar.gz"
+EXECUTOR_FILE="executor-linux-v0.21.1.tar.gz"
 
 echo "Downloading the Executor binary from $EXECUTOR_URL..."
 curl -L -o $EXECUTOR_FILE $EXECUTOR_URL
