fcf1f51a14c90d20eacab395b4b8a11bd239b9a8
tyaga321 <58379335+tyaga321@users.noreply.github.com>
2024-09-22 11:13:10 +0700

    Create installxx.sh

---
+++ installxx.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+echo "Welcome to the t3rn ALE!"
+
+sleep 1
+
+cd $HOME
+rm -rf executor
+sudo apt -q update
+sudo apt -qy upgrade
+
+
+EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.21.1/executor-linux-v0.21.1.tar.gz"
+EXECUTOR_FILE="executor-linux-v0.21.1.tar.gz"
+
+echo "Downloading the Executor binary from $EXECUTOR_URL..."
+curl -L -o $EXECUTOR_FILE $EXECUTOR_URL
+
+if [ $? -ne 0 ]; then
+    echo "Failed to download the Executor binary. Please check your internet connection and try again."
+    exit 1
+fi
+
+echo "Extracting the binary..."
+tar -xzvf $EXECUTOR_FILE
+rm -rf $EXECUTOR_FILE
+cd executor/executor/bin
+
+echo "Binary downloaded and extracted successfully."
+echo
+
+export NODE_ENV=testnet
+export LOG_LEVEL=debug
+export LOG_PRETTY=false
+
+
+read -p "Enter your Private Key from Metamask: " PRIVATE_KEY_LOCAL
+export PRIVATE_KEY_LOCAL=$PRIVATE_KEY_LOCAL
+echo -e "\nPrivate key has been set."
+echo
+
+export ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,blast-sepolia,optimism-sepolia,l1rn'
+
+
+export RPC_ENDPOINTS_ARBT='https://rpc.ankr.com/arbitrum_sepolia/d12e48988175351fc0875c59fb25155e53a8d2078157e33b49831fa5b8949999'
+
+export RPC_ENDPOINTS_BSSP='https://rpc.ankr.com/base_sepolia/d12e48988175351fc0875c59fb25155e53a8d2078157e33b49831fa5b8949999'
+
+export RPC_ENDPOINTS_BLSS='https://rpc.ankr.com/blast_testnet_sepolia/d12e48988175351fc0875c59fb25155e53a8d2078157e33b49831fa5b8949999'
+export RPC_ENDPOINTS_OPSP='https://rpc.ankr.com/optimism_sepolia/d12e48988175351fc0875c59fb25155e53a8d2078157e33b49831fa5b8949999'
+
+sleep 2
+echo "Starting the Executor..."
+./executor
