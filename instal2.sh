8b8ac1b986856c018b7b129d3ee24c2984788d9c
tyaga321 <58379335+tyaga321@users.noreply.github.com>
2024-09-22 17:46:17 +0700

    Update instal2.sh

--- instal2.sh
+++ instal2.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 echo "Welcome to the t3rn ALE!"
-mkdir t3rn2
-sleep 1
-cd t3rn2
 
 sleep 1
-rm run_executor2.sh; wget -O run_executor2.sh https://raw.githubusercontent.com/tyaga321/t3rns/main/run_executor2.sh
+rm run_executor.sh; wget -O run_executor.sh https://raw.githubusercontent.com/tyaga321/t3rns/main/run_executor.sh
 
 
+cd $HOME
+rm -rf executor
 sudo apt -q update
 sudo apt -qy upgrade
 
 
-EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.21.0/executor-linux-v0.21.0.tar.gz"
-EXECUTOR_FILE="executor-linux-v0.21.0.tar.gz"
+EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.21.0/executor-linux-v0.19.0.tar.gz"
+EXECUTOR_FILE="executor-linux-v0.19.0.tar.gz"
 
 echo "Downloading the Executor binary from $EXECUTOR_URL..."
 curl -L -o $EXECUTOR_FILE $EXECUTOR_URL
@@ -31,39 +30,21 @@ cd t3rn2/executor/executor/bin
 echo "Binary downloaded and extracted successfully."
 echo
 
-read -p "Enter your preferred Node Environment (e.g., testnet, mainnet): " NODE_ENV
-export NODE_ENV=${NODE_ENV:-testnet}
-echo "Node Environment set to: $NODE_ENV"
-echo
-
+export NODE_ENV=testnet
 export LOG_LEVEL=debug
 export LOG_PRETTY=false
-echo "Log settings configured: LOG_LEVEL=$LOG_LEVEL, LOG_PRETTY=$LOG_PRETTY"
-echo
+
 
 read -s -p "Enter your Private Key from Metamask: " PRIVATE_KEY_LOCAL
 export PRIVATE_KEY_LOCAL=$PRIVATE_KEY_LOCAL
 echo -e "\nPrivate key has been set."
 echo
 
-read -p "Enter the networks to operate on (comma-separated, e.g., arbitrum-sepolia,base-sepolia): " ENABLED_NETWORKS
-export ENABLED_NETWORKS=${ENABLED_NETWORKS:-arbitrum-sepolia,base-sepolia,optimism-sepolia,l1rn}
-echo "Enabled Networks set to: $ENABLED_NETWORKS"
-echo
+export ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,blast-sepolia,optimism-sepolia,l1rn'
 
-read -p "Would you like to set custom RPC URLs? (y/n): " SET_RPC
-if [ "$SET_RPC" == "y" ]; then
-  for NETWORK in $(echo $ENABLED_NETWORKS | tr "," "\n"); do
-    read -p "Enter the RPC URLs for $NETWORK (comma-separated): " RPC_URLS
-    export EXECUTOR_${NETWORK^^}_RPC_URLS=$RPC_URLS
-    echo "RPC URLs set for $NETWORK"
-  done
-else
-  echo "Skipping custom RPC URL setup. Default URLs will be used."
-fi
-echo
+
 
 echo "Starting the Executor..."
-cd t3rn2
-chmod +x run_executor2.sh
-./run_executor2.sh
+cd
+chmod +x run_executor.sh
+./run_executor.sh
